0aea736acee170f8b9ae07be8b8f85c0b9f081ea
ThyRex3 <kathydai@email.uark.edu>
2016-03-29 21:40:54 -0500

    Add DH-Key Exchange
    Unsure if done correctly

--- client.py
+++ client.py
@@ -4,6 +4,26 @@ HOST = '127.0.0.1'    # The remote host
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+# Variables Used
+sharedPrime = 23    # p
+sharedBase = 5      # g
+ 
+aliceSecret = 6     # a
+bobSecret = 15      # b
+ 
+# Begin
+ 
+# Alice Sends Bob A = g^a mod p
+A = (sharedBase**aliceSecret) % sharedPrime
+ 
+# Bob Sends Alice B = g^b mod p
+B = (sharedBase ** bobSecret) % sharedPrime
+ 
+# Alice Computes Shared Secret: s = B^a mod p
+aliceSharedSecret = (B**aliceSecret) % sharedPrime
+ 
+# Bob Computes Shared Secret: s = A^b mod p
+bobSharedSecret = (A**bobSecret) % sharedPrime
 s.sendall('Hello, world')
 data = s.recv(1024)
 s.close()
