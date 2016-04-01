aca110aa70e0bed865443b416f475cd11af3277f
ThyRex3 <kathydai@email.uark.edu>
2016-04-01 08:49:59 -0500

    DH and RSA Works - Need exchange of secret keys

--- client.py
+++ client.py
@@ -1,33 +1,100 @@
 import socket
+import rsa
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+import ast
 from random import getrandbits
+import sys
+import time
+from socket import error as socket_error
 
+def RSAEnc(plaintext, publicKey):
+   holder = rsa.PublicKey.load_pkcs1_openssl_pem(publicKey)
+   cipherText = rsa.encrypt(plaintext, holder)
+   return cipherText
+
+def RSADec(cipherText, privateKey):
+   holder = rsa.PrivateKey.load_pkcs1(privateKey, "PEM")
+   plaintext = rsa.decrypt(cipherText, holder)
+   return plaintext
+
+def DHCalc(g, p, x):
+   return ((int(g)**int(x)) % int(p))
+
+bobPrivateKey =  """-----BEGIN RSA PRIVATE KEY-----
+MIICXgIBAAKBgQDkr8IivGI753PxologDYiEG18VDRlCeNBJ9TCxlHRkVVfNTyBw
+AlUqFNkLodLoNwQFKQrAQvS4d0uhMGfY7chS++qNEWa2+55yI6dYKDwkOXbyRfet
+aDiRJqvxBIUCpl9tTc0BafSfp8XDnFNtLIbVZoeiG1BX5485bHGRqhXL7QIDAQAB
+AoGAV52zHoXYiST7Oge+yesFc7/c5P7Yv6vz+XH0TwUWtt4vvpxjTCbIpE/KfHdq
+i8eQRb3cvZ6pjgc+taoLD4TCq7cpBxz476Vno9q7Wu+zyyMbWBSv+YFWm77HlBwS
+Z0Bl0G7rwdhsjhwsgHHiZOCs7E6OSvTj1A3VOm3AYsMoJK0CQQD0TjyRGpbNV/n+
+ireTtc2nz9MzbG4Du473mW+hEBJoBRX2DKaBhS/mubWEYKoiyfZOF3hZX8eW0TX2
+ec/04fnnAkEA76IfhZFl7M6GuIbwixOEfZ+1EaqjQ9IUTr8d/uSTjtDDoeQ3GW5X
+FWi0tIsYkVnNL+wtMQ2PeP3LUGtjqzOZCwJBANerRX6XaW9HbhNOZDdKtI2jQwBP
+hWNYLSLZWhlmdclMTBHVIxyN9jaJ1PtS1n81qXFQ+NZ1Xl3+vNOkv3egEhsCQQC+
+84KIzc7Zf80Mt8JwIJJgBGal+EJ3Ja02/sYpOf13PVXW6GMbqbhNAA2XHIvsLxH5
+UQrF3tdoA10C7UATyV73AkEAsyGXpS6EMmKwCe0b5YtfZ7AU+6EmZMgoTbG2YONJ
+g3EvCnfEowdQ9P9KyCUT7Fk5cvGLpLNJ+CTDTCfJYRe1Lg==
+-----END RSA PRIVATE KEY----- """
+
+bobPublicKey = """-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDkr8IivGI753PxologDYiEG18V
+DRlCeNBJ9TCxlHRkVVfNTyBwAlUqFNkLodLoNwQFKQrAQvS4d0uhMGfY7chS++qN
+EWa2+55yI6dYKDwkOXbyRfetaDiRJqvxBIUCpl9tTc0BafSfp8XDnFNtLIbVZoei
+G1BX5485bHGRqhXL7QIDAQAB
+-----END PUBLIC KEY-----"""
+
+alicePrivateKey = """-----BEGIN RSA PRIVATE KEY----- 
+MIICXgIBAAKBgQDAYIgVWXVsT3Y3zm3ZEtFKBnWMotD4eydxHSJSCw4gz1TOHEbB 
+UmDWbW4smzOfBorkGLVtXUUjrWU2fAtCak052cpkzX5QNO21ThghFKqzVc+AFt3q 
+AqjkufwXQUgU5U2qVMUofIMWi4g4CS135uEmJWtZLu2h3LRpY6W54rgFFwIDAQAB 
+AoGBAIE6Vy/AQFjZqBgk2zWOpniLjjtCxA2m7P/XCk8CjiMMI3OxGvaSV+qy5+ee 
++jZBNtuynW0x0lf9CphnC0k6D1U6sEAvofqs4lMGdCjdcuLLvFVNOX1aNVrJmCYw 
+LX4DpCoYzmOjGJDIUOUeA22lleCdH5Q1g6gO3fh6SE1ETXZBAkEA6Ob1nt8ac2Rn 
++ssoQWjxuGZC6aqKeq/peYLG38GGDzKQkR9+rm4WwVO4yh4WpLU3ak/FWhLi7fPz 
+HSm9RW/yHQJBANN0syYMtG/RaKDO0EAXj6kL0hQKU9VF8XE1uLk9t9BfCHn03HTs 
+1G2S5radDw02FZqunqQaKrR5PltSHw8ZrcMCQQCq00RSy+c1ve56R+p114h8LR1l 
+D/5UMJS52E8QLXyrxvW8S/J59Ctij4rZTKplErnbkzj4cSPbTnQB7uxxcsONAkEA 
+0AatAC/bi23+esU7hvIm+O2SHPkUBGss3m01b7fSEAKOOjy0batYSPwOUXUuC5c7 
+pdNIarT7clUdDYY63AYtNQJAGbFOol0mHNGOh3ufF38XQ1sSILQdeFbnxNOvtmV9 
+RA83pgJxFCREi70oz9yci8olUY5eJvqPBrcRI4UjqtV9uA==
+ -----END RSA PRIVATE KEY-----"""
+
+alicePublicKey = """-----BEGIN PUBLIC KEY----- 
+MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgH1LaxDblUH8MtKCq4HuqLHqcfFo 
+67939uV3Svfby3zfqDuWNJmWw7lNS0iRTQ8eQ4px6pDHGaN5oYrUU/TArYx1nTOd 
+M40nCEwDh+jf612jCAOykrgN+4RKjXqsctrPpHc8CfEEIBEVIUXBomd8iEzn/S62 
+TJCV9FmE9a+HiwDhAgMBAAE=
+ -----END PUBLIC KEY-----"""
+
+DH_A = 29
+
+h = SHA256.new('AliceSecret').hexdigest()
 HOST = '127.0.0.1'    # The remote host
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-sharedPrime = 23	#p
-sharedBase = 5		#g
+s.sendall('Requesting RSA information')
+#data = s.recv(1024)
+s.sendall(RSAEnc(h, alicePublicKey))
 
-aliceSecret = 6		#a
-bobSecret = 15		#b
+g = int(s.recv(2))
+p = int(s.recv(2))
 
-# Alice sends Bob: A = g^a mod p
-A = (sharedBase**aliceSecret) % sharedPrime
+print(DHCalc(g, p, DH_A))
+print(h)
+print("g = ", g)
+print("p = ", p)
 
-# Bob sends Alice: B = g^b mod p
-B = (sharedBase**bobSecret) & sharedPrime
+exponB = s.recv(10)
+s.sendall(str(DHCalc(g, p, DH_A)))
+print("rec'd bob number", exponB)
 
-#Alice computes shared Secret Key: s = B^a mod p
-aliceSharedSecret = (B ** aliceSecret) % sharedPrime
+sharedSecret = DHCalc(exponB, p, DH_A)
+print "Shared secret = ", int(sharedSecret)
 
-#Bob computes shared Secret Key: s = A^b mod p
-bobSharedSecret = (A ** bobSecret) % sharedPrime
 
-#Send shared Secret Key from Bob to Alice
-s.sendall('%d' %bobSharedSecret)
-data = s.recv(1024)
+
 s.close()
-print 'Received', repr(data)
-
-# https://github.com/lowazo/pyDHE/blob/master/DiffieHellman.py
-# https://github.com/lowazo/pyDHE/blob/master/simpleDH.py
+#print 'Received', repr(data)
--- server.py
+++ server.py
@@ -1,22 +1,102 @@
 import socket
+import rsa
+import Crypto
+from Crypto.PublicKey import RSA
+import ast
+from random import getrandbits
+import sys
+import time
+from socket import error as socket_error
 
+def RSAEnc(plaintext, publicKey):
+   holder = rsa.PublicKey.load_pkcs1_openssl_pem(publicKey)
+   cipherText = rsa.encrypt(plaintext, holder)
+   return cipherText
+
+def RSADec(cipherText, privateKey):
+   holder = rsa.PrivateKey.load_pkcs1(privateKey, "PEM")
+   plaintext = rsa.decrypt(cipherText, holder)
+   return plaintext
+
+def DHCalc(g, p, x):
+   return ((int(g)**int(x)) % int(p))
+
+bobPrivateKey =  """-----BEGIN RSA PRIVATE KEY-----
+MIICXgIBAAKBgQDkr8IivGI753PxologDYiEG18VDRlCeNBJ9TCxlHRkVVfNTyBw
+AlUqFNkLodLoNwQFKQrAQvS4d0uhMGfY7chS++qNEWa2+55yI6dYKDwkOXbyRfet
+aDiRJqvxBIUCpl9tTc0BafSfp8XDnFNtLIbVZoeiG1BX5485bHGRqhXL7QIDAQAB
+AoGAV52zHoXYiST7Oge+yesFc7/c5P7Yv6vz+XH0TwUWtt4vvpxjTCbIpE/KfHdq
+i8eQRb3cvZ6pjgc+taoLD4TCq7cpBxz476Vno9q7Wu+zyyMbWBSv+YFWm77HlBwS
+Z0Bl0G7rwdhsjhwsgHHiZOCs7E6OSvTj1A3VOm3AYsMoJK0CQQD0TjyRGpbNV/n+
+ireTtc2nz9MzbG4Du473mW+hEBJoBRX2DKaBhS/mubWEYKoiyfZOF3hZX8eW0TX2
+ec/04fnnAkEA76IfhZFl7M6GuIbwixOEfZ+1EaqjQ9IUTr8d/uSTjtDDoeQ3GW5X
+FWi0tIsYkVnNL+wtMQ2PeP3LUGtjqzOZCwJBANerRX6XaW9HbhNOZDdKtI2jQwBP
+hWNYLSLZWhlmdclMTBHVIxyN9jaJ1PtS1n81qXFQ+NZ1Xl3+vNOkv3egEhsCQQC+
+84KIzc7Zf80Mt8JwIJJgBGal+EJ3Ja02/sYpOf13PVXW6GMbqbhNAA2XHIvsLxH5
+UQrF3tdoA10C7UATyV73AkEAsyGXpS6EMmKwCe0b5YtfZ7AU+6EmZMgoTbG2YONJ
+g3EvCnfEowdQ9P9KyCUT7Fk5cvGLpLNJ+CTDTCfJYRe1Lg==
+-----END RSA PRIVATE KEY----- """
+
+bobPublicKey =  """-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDkr8IivGI753PxologDYiEG18V
+DRlCeNBJ9TCxlHRkVVfNTyBwAlUqFNkLodLoNwQFKQrAQvS4d0uhMGfY7chS++qN
+EWa2+55yI6dYKDwkOXbyRfetaDiRJqvxBIUCpl9tTc0BafSfp8XDnFNtLIbVZoei
+G1BX5485bHGRqhXL7QIDAQAB
+-----END PUBLIC KEY-----"""
+
+alicePrivateKey =  """-----BEGIN RSA PRIVATE KEY-----
+MIICWgIBAAKBgH1LaxDblUH8MtKCq4HuqLHqcfFo67939uV3Svfby3zfqDuWNJmW
+w7lNS0iRTQ8eQ4px6pDHGaN5oYrUU/TArYx1nTOdM40nCEwDh+jf612jCAOykrgN
++4RKjXqsctrPpHc8CfEEIBEVIUXBomd8iEzn/S62TJCV9FmE9a+HiwDhAgMBAAEC
+gYB2RMhtgzhirtKyTtHhtgva1Th07dsKQwz1ESPczsZHuz6r8F76U4uw0Dst5qnc
+iW6rslf+DVIwM1G/ICmXsICf4H4b8RNYOzyQtlyAXpLiofNxLGywoNrNwC/4h2nC
+ppPDQocWv//ncnHRbBVHt/JrDY3DEd8rLgUImBip/YnbIQJBAPSiCT02T4lvVn7U
+cKqYDZrz4j63U0DAj8AdNeCXYdoeCTMUFC0tyC4pgpsWIKznhMkP6g7zowzkWfMZ
+sKmf9dUCQQCDHdNeHQiV9HsMH8oIkby3BLcXjMeCSf/XpIxfuUxyXa1VFglOAD/f
+AkYkl4AWaHKkMQI2KloLIobefHOSoKjdAkB4CaJ69NFmYMmShm+aZe4XIKDdoVsq
+pNJktHCheea7/o0JEUstOA/IBvpdWyhBb4FKn5J8L7TMOKiijvKO6TzZAkBO6mOu
+kID7aGOq/3MlRzozWaYtiXEHTSysiQBoGOoXJ6TMwm+lP+cxfXfkTD8uvXzKsCip
+m0II06YjQPp6tTINAkAJHfsmZeSYIV13BXE2heZF2AY1w7kWimwP29OPQCAP30H8
+VADrZltqk9Uz0HJI6OKurPQXTP8W4mJmbauslg3Z
+-----END RSA PRIVATE KEY-----"""
+
+alicePublicKey = """-----BEGIN PUBLIC KEY-----
+MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgH1LaxDblUH8MtKCq4HuqLHqcfFo
+67939uV3Svfby3zfqDuWNJmWw7lNS0iRTQ8eQ4px6pDHGaN5oYrUU/TArYx1nTOd
+M40nCEwDh+jf612jCAOykrgN+4RKjXqsctrPpHc8CfEEIBEVIUXBomd8iEzn/S62
+TJCV9FmE9a+HiwDhAgMBAAE=
+-----END PUBLIC KEY-----"""
+
+
+DH_B = 47
+g = "13"
+p = "31"
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
-conn, addr = s.accept()
 
-dataRecieved = []
+connected = False
 
-print 'Connected by', addr
-while 1:
+while not connected:
+    conn, addr = s.accept()
     data = conn.recv(1024)
-    if not data: break
-    dataRecieved += data
+    hash = conn.recv(10000)
+    print(hash)
+    print(RSADec(hash, alicePrivateKey))
+
+    conn.sendall(g)
+    conn.sendall(p)
+    conn.sendall(str(DHCalc(int(g), int(p), DH_B)))
+    exponA = conn.recv(10)
+    print("bob num = ", DHCalc(int(g), int(p), DH_B))
+    print("received alice number, ", exponA)
+
+    sharedSecret = DHCalc(exponA, p, DH_B)
+    print "Shared secret", sharedSecret
+
 
     conn.sendall(data)
 conn.close()
 
-for x in dataRecieved:
-   print 'asdf', x
